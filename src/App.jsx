9369e923628646d2c31d012db5c14e7f2a291416
CodeWith-Het <hetp1065@gmail.com>
2025-08-22 09:49:48 -0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import Navbar from './Components/Navbar'
-// import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
 import IntegratingAPI from './API-Call/IntegratingAPI'
 import TwoWay from './TwoWayBlindingMiniProject'
 import FormHandlingProject from './form-handling-project'
