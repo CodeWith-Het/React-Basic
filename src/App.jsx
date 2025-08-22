19ed6e8b41181497179a2c62107e2eec6af584e5
Het Patel <hetp1065@gmail.com>
2025-08-22 09:31:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-// import Navbar from './Components/Navbar'
-// import Hero from './Components/Hero'
-import IntegratingAPI from './API-Call/IntegratingAPI'
-import TwoWay from './TwoWayBlindingMiniProject'
 import FormHandlingProject from './form-handling-project'
+import TwoWay from './TwoWayBlindingMiniProject'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import IntegratingAPI from './API-Call/IntegratingAPI'
 
 const App = () => {
   return (
