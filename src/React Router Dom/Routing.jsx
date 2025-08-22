1b5fae78d50320826ecf991e30cff02eb6a0147d
CodeWith-Het <hetp1065@gmail.com>
2025-08-22 09:58:39 -0400

    Implemented routing logic in Routing.jsx

--- src/React Router Dom/Routing.jsx
+++ src/React Router Dom/Routing.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './header.css'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Product from './Product'
 
 const Routing = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to='/Product'>Product</Link>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to='/Product'>Product</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Product" element={<Product />} />
+      </Routes>
+    </Router>
   )
 }
 
